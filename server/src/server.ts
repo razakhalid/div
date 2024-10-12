a7a0066067d7497345d3911d7c4338d2d961761a
Raza Khalid <rkhalid@kigo.io>
2024-10-11 22:33:44 -0500

    serve static client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import path from "path";
 
 const app = express();
-
 const port = process.env.PORT || 8080;
+const clientPath = path.join(__dirname, '..', '..', '..', 'client');
+
+app.use(express.static(`${clientPath}/dist`));
+
+app.get("/*", (_, res) => {
+  res.sendFile(`${clientPath}/dist/index.html`);
+})
 
 app.listen(port, () => {
   console.log("Listening on port ", port);
